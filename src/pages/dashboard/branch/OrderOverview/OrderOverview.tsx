6db7c57ca980c05aeb5bb01fbdb5afe32464012c
sahil98mallick <sahil98mallick@gmail.com>
2024-02-16 15:01:57 +0530

    Order_preview

--- src/pages/dashboard/branch/OrderOverview/OrderOverview.tsx
+++ src/pages/dashboard/branch/OrderOverview/OrderOverview.tsx
@@ -66,7 +66,7 @@ export default function OrderOverview() {
         page: 1,
         length: 5,
         keyword: "",
-        sortOrder: sortkey,
+        sortOrder: "",
         orderStatus: orderStatus,
         actionPendingBy: ""
       }
